7e3fb30e8b103238fc2708941c6ed4ed8e059acd
joshfermin <joshfermin@gmail.com>
2014-10-08 12:57:11 -0600

    starting to do createChildren

--- Assignment3/gerrymander2.py
+++ Assignment3/gerrymander2.py
@@ -4,20 +4,29 @@ import sys
 import numpy as np # for numpy array
 
 class Node:
-	def __init__(self, i_depth, i_player, i_move, i_movesRemaining, i_value = 0):
+	def __init__(self, i_depth, i_player, i_move, i_movesRemaining, i_totalDistrict, i_value = 0):
 		self.i_depth = i_depth
 		self.i_player = i_player # true player (MAX), false player (MIN)
-		self.i_value = i_value
 		self.i_move = i_move
 		self.i_movesRemaining = i_movesRemaining
+		self.i_totalDistrict = i_totalDistrict
+		self.i_value = i_value
 		self.children = []
 		self.CreateChildren()
 
 
 	def CreateChildren(self):
-		# if self.i_depth >= 0: # how far down the tree you want to calculate
-		# 	if self.i_move = 
-		# 	self.children.append(Node(self.i_depth))
+		print self.i_movesRemaining
+		for key in self.i_move:
+			moveKey = (key[0], None)
+		if self.i_depth >= 0: # how far down the tree you want to calculate
+			if moveKey == ('BOT RIGHT', None):
+				for i in range((self.i_totalDistrict / 2), self.i_totalDistrict):
+					del self.i_movesRemaining['BOT ROW ' + str(i), None]
+					del self.i_movesRemaining['LEFT COL ' + str(i), None]
+				print self.i_movesRemaining
+			print "\n"
+			# self.children.append(Node(self.i_depth))
 		pass
 
 	def Score(self, choice, player):
@@ -54,25 +63,25 @@ class Node:
 				score = -2
 
 def main():
-	moveDictionary = dict()
 	neighborhoodMatrix = convertToNeighborhoodMatrix(sys.argv[1])
 	neighborhoodMatrix = np.asarray(neighborhoodMatrix)
 	totalNeighborhoods = len(list(itertools.chain(*neighborhoodMatrix))) # get number of choices
 	totalDistricts = len(neighborhoodMatrix)
-	allMoves = getAllMoves(neighborhoodMatrix, totalDistricts)
+
+	######### INITIALIZE FIRST MOVE OF GAME TREE #############
+	moveDictionary = dict() # dictionary with total moves left
+	allMoves = getAllMoves(neighborhoodMatrix, totalDistricts) 
 	moveDictionary = allMoves[1] # dictionary with moves (top row/ bot row, left col/ right col)
-	print moveDictionary
-	allMoves = allMoves[0] 
-	allMoves = np.asarray(allMoves)
-	init_move = firstMove(allMoves, totalDistricts)
-	# print init_move[0]
-	allMoves = np.delete(allMoves, init_move[0], axis=0)
-	init_move = init_move[1]
+	init_move = firstMoveDictionary(moveDictionary, totalDistricts) # get first move as a dicitonary
+	i_move = init_move[0] # first 
+	i_movesRemaining = init_move[1]
 	print "Total Neighborhoods: " + str(totalNeighborhoods) + "\n" + "Total Districts should be: " + str(totalDistricts)
 	i_depth = 4 # depth of tree you want to calculate
 	i_curPlayer = "MAX" # max will start
-	max_init_node = Node(i_depth, i_curPlayer, init_move, allMoves)
-	print "*"*30 + "\n MAX = R \n MIN = D \n" + "*"*30
+	for key in i_move:
+		i_value = key[1]
+	max_init_node = Node(i_depth, i_curPlayer, i_move, i_movesRemaining, totalDistricts, i_value)
+	print "*"*30 + "\n MAX = D \n MIN = R \n" + "*"*30
 
 def getAllMoves(matrix, lengthOfChoice):
 	choices = []
@@ -103,6 +112,10 @@ def getAllMoves(matrix, lengthOfChoice):
 		# bottom right square
 		choices.append([matrix[-1][-1], matrix[-2][-1], matrix[-1][-2], matrix[-2][-2]])
 		choiceDictionary["BOT RIGHT"] = [matrix[-1][-1], matrix[-2][-1], matrix[-1][-2], matrix[-2][-2]]
+	if lengthOfChoice == 8: # for large neighborhood [8 x 8]
+		choices.append([matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3], matrix[1][0], matrix[1][1]]) 
+		choiceDictionary["TOP LEFT"] = [matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1]]
+
 	for i in range(0, (lengthOfChoice / 2)):
 		choiceDictionary["LEFT COL " + str(i)] = columnarray[i]
 		choiceDictionary["TOP ROW " + str(i)] = rowarray[i]
@@ -113,7 +126,7 @@ def getAllMoves(matrix, lengthOfChoice):
 	returnarray = []
 	returnarray.append(choices)
 	returnarray.append(choiceDictionary)
-	return returnarray
+	return returnarray # returns choices and choice dictionary 
 
 def firstMove(choices, lengthOfChoice):
 	scorearray = []
@@ -146,6 +159,40 @@ def firstMove(choices, lengthOfChoice):
 	returnarray.append(bestmove)
 	return returnarray
 
+def firstMoveDictionary(choices, lengthOfChoice):
+	newChoiceDictionary = dict()
+	scorearray = []
+	returnList = []
+	score = 0
+	size = lengthOfChoice
+	for choice in choices:
+		d = 0 
+		r = 0
+		for i in choices[choice]:
+			if i == "D":
+				d = d + 1
+			if i == "R":
+				r = r + 1
+		if r == size:
+			score = 2
+		elif r > (size / 2):
+			score = 1
+		elif r == d:
+			score =0
+		elif d > (size / 2):
+			score = -1
+		elif d == size:
+			score = -2
+		newChoiceDictionary[(choice,score)] = choices[choice] 
+	for key in newChoiceDictionary:
+		if(key[1] ==  max(k[1] for k, v in newChoiceDictionary.iteritems() if v != 0)):
+			bestkey = key
+	bestchoice = {bestkey: newChoiceDictionary[bestkey]}
+	del newChoiceDictionary[bestkey]
+	returnList.append(bestchoice)
+	returnList.append(newChoiceDictionary)
+	return returnList
+
 def convertToNeighborhoodMatrix(inputtxt):
 	Neighborhood = open(inputtxt, 'r')
 	neighborhoodMatrix = map(lambda line: line.rstrip('\n'), Neighborhood)
@@ -174,6 +221,17 @@ def minimaxSearch(node, depth, player):
 
 if __name__ == "__main__":
 	main()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
