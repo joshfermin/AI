1f9a5c5a944d6d6ca5fa8880202753667ff097a4
joshfermin <joshfermin@gmail.com>
2014-11-08 23:53:53 -0700

    using library

---
+++ Assignment4/CancerModel.py
@@ -0,0 +1,87 @@
+import sys
+from numpy import *
+sys.path.append('lib/')
+from pbnt.Graph import *
+from pbnt.Distribution import *
+from pbnt.Node import *
+
+
+def cancer():
+    """ This is an example of how to implement the basic cancer network.
+    """
+
+    #testing basic bayes net class implementation
+    numberOfNodes = 5
+    #name the nodes
+    pollution = 0
+    smoker = 1
+    cancer = 2
+    xray = 3
+    dysponea = 4
+
+    pNode = BayesNode(0, 2, name="pollution")
+    sNode = BayesNode(1, 2, name="smoker")
+    cNode = BayesNode(2, 2, name="cancer")
+    xNode = BayesNode(3, 2, name="xray")
+    dNode = BayesNode(4, 2, name="dysponea")
+
+    #Pollution
+    pNode.add_child(cNode)
+
+    #Smoker
+    sNode.add_child(cNode)
+
+    #Cancer
+    cNode.add_parent(pNode)
+    cNode.add_parent(sNode)
+    cNode.add_child(xNode)
+    cNode.add_child(dNode)
+
+    #Xray
+    xNode.add_parent(cNode)
+
+    #Dysponea
+    dNode.add_parent(cNode)
+
+    nodes = [pNode, sNode, cNode, xNode, dNode]
+
+    #create distributions
+    #pollution distribution
+    pDistribution = DiscreteDistribution(pNode)
+    index = pDistribution.generate_index([],[])
+    pDistribution[index] = [0.10, 0.90]
+    pNode.set_dist(pDistribution)
+
+    #smoker distribution
+    sDistribution = DiscreteDistribution(sNode)
+    index = sDistribution.generate_index([],[])
+    sDistribution[index] = [0.70, 0.30]
+    sNode.set_dist(sDistribution)
+
+    #Cancer
+    dist = zeros([pNode.size(), sNode.size(), cNode.size()], dtype=float32)
+    dist[0,0,] = [0.98,0.02]
+    dist[1,0,] = [0.999,0.001]
+    dist[0,1,] = [0.95,0.05]
+    dist[1,1,] = [0.97,0.03]
+    cDistribution = ConditionalDiscreteDistribution(nodes=[pNode, sNode, cNode], table=dist)
+    cNode.set_dist(cDistribution)
+
+    #x-ray
+    dist = zeros([cNode.size(), xNode.size()], dtype=float32)
+    dist[0,] = [0.1,0.9]
+    dist[1,] = [0.8,0.2]
+    xDistribution = ConditionalDiscreteDistribution(nodes=[cNode, xNode], table=dist)
+    xNode.set_dist(xDistribution)
+
+    #dysponea
+    dist = zeros([cNode.size(), dNode.size()], dtype=float32)
+    dist[0,] = [0.35,0.65]
+    dist[1,] = [0.7,0.3]
+    dDistribution = ConditionalDiscreteDistribution(nodes=[cNode, dNode], table=dist)
+    dNode.set_dist(dDistribution)
+
+
+    #create bayes net
+    bnet = BayesNet(nodes)
+    return bnet
---
+++ Assignment4/bayes.py
@@ -0,0 +1,142 @@
+import getopt
+import sys
+import CancerModel as Cancer
+from numpy import *
+sys.path.append('lib/')
+from pbnt.Graph import *
+from pbnt.Distribution import *
+from pbnt.Node import *
+from pbnt.Inference import *
+
+class Node:
+	def __init__(self, children, parents, probability, value):
+		self.children = children
+		self.parents = parents
+		self.probability = probability
+		self.value = value
+
+def main():
+	BayesNet = Cancer.cancer()
+	engine = JunctionTreeEngine(BayesNet)
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hgjm:v", ["help", "conditional=", "joint=", "marginal="])
+	except getopt.GetoptError as err:
+        # print help information and exit:
+		print str(err) # will print something like "option -a not recognized"
+		sys.exit(2)
+	output = None
+	verbose = False
+	for o, a in opts:
+		arglookup = findArgValue(a)
+		for node in BayesNet.nodes:
+			if node.id == 0 and arglookup == 'p':
+				toCalculate = node
+			if node.id == 1 and arglookup == 's':
+				toCalculate = node
+			if node.id == 2 and arglookup == 'c':
+				toCalculate = node
+			if node.id == 3 and arglookup == 'x':
+				toCalculate = node
+			if node.id == 4 and arglookup == 'd':
+				toCalculate = node
+
+		if o == "-v":
+			verbose = True # what does this do?
+			# somehow makes args work
+		elif o in ("-h", "--help"):
+			print "-g is flag for conditional probability\n-j is flag for joint probability\n-m is flag for marginal probability"
+			sys.exit()
+		elif o in ("-g", "--conditional"):
+			args = a
+			conditionalProbablity(args, BayesNet)
+		elif o in ("-j", "--joint"):
+			args = a
+			jointProbability(args, BayesNet)
+		elif o in ("-m", "--marginal"):
+			args = a
+			marginalProbability(args, BayesNet, engine, toCalculate)
+		else:
+			assert False, "unhandled option"
+
+def conditionalProbablity(args, BayesNet):
+	print "\nThe conditional probability for " + args + " is:\n"
+	argtype = checkArgs(args)
+	arglookup = findArgValue(args)
+	for node in BayesNet:
+		if node.value == arglookup:
+			if argtype == "lower":
+				return
+			if argtype == "upper":
+				return
+			if argtype == "squiggle":
+				return
+
+def jointProbability(args, BayesNet):
+	print "\nThe joint probability for " + args + " is:\n"
+	argtype = checkArgs(args)
+	arglookup = findArgValue(args)
+	for node in BayesNet:
+		if node.value == arglookup:
+			if argtype == "lower":
+				return
+			if argtype == "upper":
+				return
+			if argtype == "squiggle":
+				return
+
+def marginalProbability(args, BayesNet, Engine, toCalculate):
+	arglookup = findArgValue(args)
+	Q = Engine.marginal(toCalculate)[0]
+	argtype = checkArgs(args)
+	
+	if argtype == "lower":
+		index = Q.generate_index([True], range(Q.nDims))
+		print "The marginal probability of " + args + "=true: ", Q[index]
+	elif argtype == "squiggle":
+		index = Q.generate_index([False], range(Q.nDims))
+		print "The marginal probability of " + args + "=false: ", Q[index]
+	# elif argtype == "upper":
+	# 	prettyPrint(node.probability, args.lower())
+	# 	inverseProbabilities = dict()
+	# 	for key in node.probability:
+	# 		inverseProbabilities[key] = (1 - node.probability[key])
+	# 	prettyPrint(inverseProbabilities, "~" + args.lower())
+	# elif argtype == "squiggle":
+	# 	inverseProbabilities = dict()
+	# 	for key in node.probability:
+	# 		inverseProbabilities[key] = (1 - node.probability[key])
+	# 	prettyPrint(inverseProbabilities, args)
+			# sum up values in dict
+	return 
+
+def prettyPrint(dictionary, args):
+	print "{:<8} {:<10}".format("P", "P(" + args + ")")
+	print "-------------"
+	for k, v in dictionary.iteritems():
+		print "{:<8} {:<10}".format(k, v)
+	print "\n"
+
+def checkArgs(args):
+	# checks the type of args
+	if args.islower():
+		if "~" in args:
+			return "squiggle"
+		else:
+			return "lower"
+	elif args.isupper():
+		return "upper"
+	
+def findArgValue(args):
+	# returns the letter of the arg in lowercase
+	if args.islower():
+		if "~" in args:
+			return args.translate(None, '~')
+		else:
+			return args
+	elif args.isupper():
+		return args.lower()
+
+if __name__ == "__main__":
+	main()
+
+#http://healthyalgorithms.com/2011/11/23/causal-modeling-in-python-bayesian-networks-in-pymc/
