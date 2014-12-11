6482d9217e9e8df20f8374c56d58d536a42c881c
joshfermin <joshfermin@gmail.com>
2014-12-11 12:18:22 -0700

    assignment5 done

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -2,17 +2,97 @@
 # viterbi.py
 
 # imports
-from __future__ import division
-# from optparse import OptionParser
 import getopt
 import sys
+import time
+from hmm import *
 
-from util import *
-from dataset import DataSet
-from hmm import HMM
+#######################
+# DATASETS - PARSING
+def list_index(xs):
+	m = {}
+	for (i, x) in enumerate(xs):
+		m[x] = i
+	return m
+
+class DataSet:
+	def __init__(self, filename, debug=False):
+		self.debug = debug
+
+		file = open(filename,"r")
+
+		states = set([])
+		outputs = set([])
+		
+		# A sequence is a list of (state, output) tuples
+		sequences = []
+		seq = []
+		switched = False
+
+		for line in file.readlines():
+			line = line.strip()
+			if len(line) == 0:
+				continue
+
+			if line ==  "." or line == "..":
+				# end of sequence
+				sequences.append(seq)
+				seq = []
+				if line == "..":
+					if switched:
+						raise Exception("File must have exactly one '..' line")
+					# Switch to test sequences
+					switched = True
+					train_sequences = sequences
+					sequences = []
+
+			else:
+				words = line.split();
+				
+				state = words[0]
+				# Keep track of all the states/outputs
+				states.add(state)
+
+				for output in words[1:]:
+					outputs.add(output)
+					seq.append( (state, output) )
+
+		# By the time we get here, better have seen the train/test
+		# divider
+		if not switched:
+			raise Exception("File must have exactly one '..' line")
+
+		# Don't forget to add the last sequence!
+		if len(seq) > 0:
+			sequences.append(seq)
+					
+		# Ok, the sequences we have now are the test ones
+		test_sequences = sequences
+
+		# Now that we have all the states and outputs, create a numbering
+		self.states = list(states)
+		self.states.sort()
+		self.outputs = list(outputs)
+		self.outputs.sort()
+		state_map = list_index(self.states)
+		output_map = list_index(self.outputs)
+
+		self.train_state = map((lambda seq: map(lambda p: state_map[p[0]], seq)),
+							   train_sequences)
+		self.train_output = map((lambda seq: map (lambda p: output_map[p[1]], seq)), 
+							   train_sequences)
+
+		self.test_state = map((lambda seq: map (lambda p: state_map[p[0]], seq)), 
+							   test_sequences)
+		self.test_output = map((lambda seq: map (lambda p: output_map[p[1]], seq)), 
+							   test_sequences)
+
+		if self.debug:
+			print self
+
 
 @print_timing
-def run_viterbi(hmm, d, debug=False):
+def viterbi(hmm, d, debug=False):
 	"""Run the viterbi algorithm for each test sequence in the given dataset"""
 	total_error = 0
 	total_n = 0
@@ -25,7 +105,7 @@ def run_viterbi(hmm, d, debug=False):
 		most_likely = [d.states[j] for j in hmm.most_likely_states(d.test_output[i])]
 		actual = [d.states[j] for j in d.test_state[i]]
 		n = len(most_likely)
-	#       print "len(most_likely) = %d  len(actual) = %d" % (n, len(actual))
+		print "len(most_likely) = %d  len(actual) = %d" % (n, len(actual))
 		for j in range(n):
 			if debug:
 				print "%s     %s      %s" % (
@@ -43,10 +123,11 @@ def run_viterbi(hmm, d, debug=False):
 	return err
 
 
+
 def train_hmm_from_data(data_filename, debug=False):
 	if debug:
 		print "\n\nReading dataset %s ..." % data_filename
-	data_filename = normalize_filename(data_filename)
+	# data_filename = normalize_filename(data_filename)
 	d = DataSet(data_filename)
 	#if options.verbose:
 	#   print d
@@ -69,52 +150,56 @@ def main(argv=None):
 		sys.exit(2)
 	output = None
 	verbose = False
+	filename = None
 	for o, a in opts:
-		if o == "-v":
-			verbose = True 
-		elif o in ("-h", "--help"):
-			print "-p is problem flag 1 - 3\n-o is hmm order flag"
+		if o in ("-h", "--help"):
+			print "-p is problem flag 1 - 3\n-o is hmm order flag\n-f allows you to specify your own dataset.\n DONT USE -p and -f in conjunction!!!"
 			sys.exit()
+		elif o in ("-o", "--option"):
+			option = int(a)
+			if option > 2 or option < 1:
+				assert False, "Not a valid hmm order flag"
+				
+				
 		elif o in ("-p", "--problem"):
 			if int(a) > 3 or int(a) < 1:
 				assert False, "Not a valid problem number"
 			else:
 				problem = int(a)
-				# print problem
+				
+					# topic change
 		elif o in ("-f", "--file"):
 			filename = a
 			print filename
-		elif o in ("-o", "--option"):
-			if int(a) > 2 or int(a) < 1:
-				assert False, "Not a valid hmm order flag"
-			else:
-				option = int(a)
+			hmm, d = train_hmm_from_data(filename, True)
+		  	err_full = viterbi(hmm, d , True)
+			# print filename
 		else:
 			assert False, "unhandled option"
 	if option == 2:
-		print "Functionality not implemented"
-		HMMorder = 2
-
-
-	if problem == 1:
-	  hmm, d = train_hmm_from_data(filename, verbose)
-	  err_full = run_viterbi(hmm, d , True)
-
-		# robot
-		# hmm = get_robot_model()
-  #       print "HMM is:"
-  #       print hmm
-		
-  #       seq = [0,1,2]
-  #       logp = hmm.log_prob_of_sequence(seq)
-  #       p = exp(logp)
-  #       print "prob ([]): logp= %f  p= %f" % (logp, p)
-  #       print "most likely states (walk, shop, clean) = %s" % hmm.most_likely_states(seq)
-
-	# elif problem == 2:
-	# 	# typo correction
-	# elif problem == 3:
-		# topic change
+		print "Extra Credit - Functionality not implemented"
+		sys.exit()
+	if filename == None:
+		if problem == 1:
+		  	hmm, d = train_hmm_from_data("Assignment5DataSets/robot_no_momemtum.data", True)
+		  	err_full = viterbi(hmm, d , True)
+		  	print "Finished robot_no_momemtum.data\nSleeping for 3 seconds...\nGetting ready to run robot_with_momentum.data"
+		  	time.sleep(3)
+		  	hmm, d = train_hmm_from_data("Assignment5DataSets/robot_with_momemtum.data", True)
+		  	err_full = viterbi(hmm, d , True)
+	  		print "Finished robot_with_momemtum.data"
+		elif problem == 2:
+			hmm, d = train_hmm_from_data("Assignment5DataSets/typos10.data", True)
+		  	err_full = viterbi(hmm, d , True)
+		  	print "Finished typos10.data\nSleeping for 3 seconds...\nGetting ready to run typos20.data"
+		  	time.sleep(3)
+		  	hmm, d = train_hmm_from_data("Assignment5DataSets/typos20.data", True)
+		  	err_full = viterbi(hmm, d , True)
+		  	print "Finished typos20.data"
+		elif problem == 3:
+			hmm, d = train_hmm_from_data("Assignment5DataSets/topics.data", False)
+		  	err_full = viterbi(hmm, d , True)
+	
 
 if __name__ == "__main__":
 	main()
--- Assignment5/hmm.py
+++ Assignment5/hmm.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python 
-
-from util import * 
 from numpy import *
 from math import log
 import copy
 import sys
+import time
 
 
 # If PRODUCTION is false, don't do smoothing 
@@ -12,6 +11,51 @@ PRODUCTION = True
 
 # Pretty printing for 1D/2D numpy arrays
 MAX_PRINTING_SIZE = 30
+
+#######################
+# HELPER FUNCTIONS
+def print_timing(func):
+    def wrapper(*arg):
+        t1 = time.time()
+        res = func(*arg)
+        t2 = time.time()
+        print '%s took %0.3f ms' % (func.func_name, (t2-t1)*1000.0)
+        return res
+    return wrapper
+
+def array_to_string(a):
+    return [str(x) for x in a]
+
+def custom_flatten(xs):
+    """flatten a list that looks like [a,[b,[c,[d,[e]]]]]
+    needed because the list can be hundreds of thousands of elements long,
+    and the recursion in regular flatten can't handle it."""
+    result = []
+    while len(xs) != 1:
+        result.append(xs[0])
+        xs = xs[1]
+    if len(xs) == 1:
+        result.append(xs[0])
+    return result
+
+def flatten(x):
+    """flatten(sequence) -> list
+    Returns a single, flat list which contains all elements retrieved
+    from the sequence and all recursively contained sub-sequences
+    (iterables).
+    Examples:
+    >>> [1, 2, [3,4], (5,6)]
+    [1, 2, [3, 4], (5, 6)]
+    >>> flatten([[[1,2,3], (42,None)], [4,5], [6], 7, MyVector(8,9,10)])
+    [1, 2, 3, 42, None, 4, 5, 6, 7, 8, 9, 10]"""
+
+    result = []
+    for el in x:
+        if hasattr(el, "__iter__") and not isinstance(el, basestring):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
 
 def format_array(arr):
     s = shape(arr)
@@ -27,33 +71,8 @@ def format_array(arr):
             lines.append("[  " + "  ".join(["%.6f" % float(arr[i,j]) for j in range(s[1])]) + "  ]")
         return "\n".join(lines)
 
-
-
 def format_array_print(arr):
     print format_array(arr)
-
-def init_random_model(N, max_obs, seed=None):
-    if seed==None:
-        random.seed()
-    else:
-        random.seed(seed)
-
-    # Initialize things to random values
-    tran_model = random.random([N,N])
-    obs_model  = random.random([N,max_obs])    
-    initial    = random.random([N])
-
-    initial    = ones([N])
-
-    # Normalize
-    initial    = initial/sum(initial)
-    for i in range(N): 
-        tran_model[i,:] = tran_model[i,:]/sum(tran_model[i,:])
-        obs_model[i,:]  = obs_model[i,:]/sum(obs_model[i,:])
-    
-    return (initial, tran_model, obs_model)
-
-
 
 def string_of_model(model, label):
     (initial, tran_model, obs_model) = model
@@ -83,295 +102,6 @@ def check_model(model):
 def print_model(model, label):
     check_model(model)
     print string_of_model(model, label)    
-
-def max_delta(model, new_model):
-    """Return the largest difference between any two corresponding 
-    values in the models"""
-    return max( [(abs(model[i] - new_model[i])).max() for i in range(len(model))] )
-
-
-def get_alpha(obs, model):
-    """ Returns the array of alphas and the log likelyhood of the sequence.
-    
-    Note: doing normalization as described in Ghahramani '01--just normalizing
-    both alpha and beta to sum to 1 at each time step."""
-    
-    (initial, tran_model, obs_model) = model
-    N = shape(tran_model)[0]
-    n = len(obs)
-    loglikelyhood = 0
-    
-    alpha = zeros((n,N))
-    alpha[0,:] = initial * obs_model[:,obs[0]]
-    normalization = sum(alpha[0,:])
-    alpha[0,:] /= normalization
-    loglikelyhood += log(normalization)
-    
-    for t in range(1,n):
-        for j in range(N):
-            s = sum(tran_model[:,j]*alpha[t-1,:])
-            alpha[t,j] = s * obs_model[j,obs[t]]
-        normalization = sum(alpha[t,:])
-        loglikelyhood += log(normalization)
-        alpha[t,:] /= normalization
-        
-    return alpha, loglikelyhood
-
-
-def get_beta(obs,model):
-    """ Note: doing normalization as described in Ghahramani '01--just normalizing
-    both alpha and beta to sum to 1 at each time step."""
-    
-    (initial, tran_model, obs_model) = model
-    N = shape(tran_model)[0]
-    n = len(obs)
-    # beta[time,state]
-    beta = zeros((n,N))
-    beta[n-1,:] = ones(N) / N
-    for t in range(n-2,-1,-1):
-        for i in range(N):
-            beta[t,i] = sum(tran_model[i,:]*obs_model[:,obs[t+1]]*beta[t+1,:])
-        normalization = sum(beta[t,:])
-        beta[t,:] /= normalization
-    return beta
-
-
-def get_gamma(alpha, beta):
-    (n,N) = shape(alpha)
-    gamma = zeros((n,N))
-    for t in range(n):
-        normalization = sum(alpha[t,:]*beta[t,:])
-        gamma[t,:] = alpha[t,:] * beta[t,:] / normalization
-    return gamma
-
-
-def get_xi(obs,alpha, beta, model):
-    (initial, tran_model, obs_model) = model
-    N = shape(tran_model)[0]
-    n = len(obs)
-    xi = zeros((n, N, N))
-    for t in range(n-1):
-        s = 0
-        for i in range(N):
-            for j in range(N):
-                xi[t,i,j] = alpha[t,i] * tran_model[i,j] * obs_model[j,obs[t+1]] * beta[t+1,j]
-                s += xi[t,i,j]
-        xi[t,:,:] = xi[t,:,:] / s
-    return xi
-
-
-def compute_expectation_step(obs, N, N_ho, N_h1h2, N_h1, N_h, model, debug=False):
-    """ E-step, update the sufficient statistics given the current model,
-    and return the loglikelihood of the dataset under the current model
-    
-    obs: the observation sequences in the training data
-    
-    N: number of hidden states
-    
-    the sufficient statistics, refer to lecture 15 notes, p13
-    all are stored in numpy arrays
-    N_ho: expected number of times in the training data that
-        an observation is the output in hidden state.
-        It is a numpy array with the number of rows
-        equal to the number of hidden states (N)
-        and the number of cols equal to the number of observations (M)
-    N_h1h2: expected number of times a transition from one hidden state to another
-    N_h1: expected number of times in each initial state
-    N_h: expected of times in each state at all (used for obs model)
-    
-    model: the current hmm model of initial, transition and observation probs
-    debug: for printing out model parameters or not, set to True by -v option in command line
-    
-    Return dataset_logliklihood
-    note get_alpha() returns the likelihood of an observation seq and
-    note that functions for getting beta, xi and gamma values are also implemented for you"""
-    
-    datasetLoglikelihood = 0.0
-    for obser in obs:
-        (alpha , loglikelihood) = get_alpha(obser , model)
-        beta = get_beta(obser , model)
-        gamma = get_gamma(alpha , beta)
-        xi = get_xi(obser , alpha , beta , model)
-        
-        datasetLoglikelihood += loglikelihood
-
-        V = shape(N_ho)[1]
-        n = len(obser)
-        
-        for i in range(N):
-            for j in range(V):
-                N_ho[i , j] += sum([gamma[t , i] for t in range(n) if obser[t] == j])
-    
-        for i in range(N):
-            for j in range(N):
-                N_h1h2[i , j] += sum(xi[:, i , j])
-    
-        N_h1 += gamma[0 , :]
-        N_h += sum(gamma , 0)
-
-    return datasetLoglikelihood
-
-
-def compute_maximization_step(N, M, N_ho, N_h1h2, N_h1, N_h, model, debug=False):
-    """M-step, update the hmm model by using the incoming sufficient statistics, and return an updated model
-    model = (initial, tran_model, obs_model)
-    
-    N: number of hidden states
-    M: number of possible observations
-    
-    the sufficient statistics, refer to lecture 15 notes, p13,
-    all are stored in numpy arrays
-    N_ho: expected number of times in the training data that
-        an observation is the output in hidden state.
-        It is a numpy array with the number of rows
-        equal to the number of hidden states (N)
-        and the number of cols equal to the number of observations (M)
-    N_h1h2: expected number of times a transition from one hidden state to another
-    N_h1: expected number of times in each initial state 
-    N_h: expected of times in each state at all (used for obs model)
-    
-    model: the current hmm model of initial, transition and observation probs
-    debug: for printing out model parameters or not, set to True by -v option in command line
-    
-    Return model, an updated hmm model of initial, transition and observation probs
-    """
-    
-    (initial , tran_model , obs_model) = model
-
-    initial = N_h1 / sum(N_h1)
-    for i in range(N):
-        tran_model[i , :] = N_h1h2[i , :] / sum(N_h1h2[i , :])
-    
-    for i in range(N):
-        obs_model[i , :] = N_ho[i , :] / sum(N_ho[i , :])
-
-    return (initial , tran_model , obs_model)
-
-
-
-# Note: This implementation is as presented in the Rabiner '89 HMM tutorial.
-# Variable definitions
-# obs    = list of numpy arrays representing multiple observation sequences
-# K = the number of observation sequences
-# N = num hidden states 
-# M = number of possible observations (assuming 0-indexed)
-# num_iters = maximum number of iterations allowed (if set to 0 then no limit)
-# For each observation sequence:
-#   n = number of observations in the sequence.  (indexed 0..n-1)
-def baumwelch(obs,N,M, num_iters=0, debug=True,init_model=None, flag=False):    
-    K = len(obs)
-
-    if debug:
-        print "K=%d N=%d  M=%d" % (K, N, M)
-
-    smoothing = PRODUCTION
-    if debug:
-        print "smoothing", PRODUCTION
-
-    if init_model == None:
-        if debug:
-            seed = 42
-        else:
-            # Just making things deterministic for now.
-            # Change to "seed = None" if you want to experiment with
-            # random restart, for example.
-            seed = 42   
-        model = init_random_model(N,M, seed)
-    else:
-        model = init_model
-
-    if debug:
-        print_model(model, "Initial model")
-    
-    # Loop variables
-    iters = 1
-    # Keep track of the likelihood of the observation sequences
-    loglikelihoods = []    
-    while True:
-        if debug:
-            print "\n\n======= Starting iteration %d ========" % iters
-        # Pull out latest parameters
-        #(initial, tran_model, obs_model) = model
-
-        if smoothing:
-            # Using prior that we've been in every state once, and seen
-            # uniform everything.
-            N_ho = ones((N,M)) / M
-            N_h1h2 = ones((N,N)) / N
-            # Number of times in each initial state (for init model)
-            N_h1 = ones(N) / N
-        
-            # Number of times in each state at all (for obs model)
-            N_h = ones(N)
-        else:
-            N_ho = zeros((N,M))
-            N_h1h2 = zeros((N,N))
-            # Number of times in each initial state (for init model)
-            N_h1 = zeros(N)
-        
-            # Number of times in each state at all (for obs model)
-            N_h = zeros(N)
-
-
-        old_model = copy.deepcopy(model)
-        
-        #### Expectation step ####
-        #N_ho, N_h1h2, N_h1, N_h are numpy arrays and are passed by reference, updated through "side-effects"
-        dataset_loglikelihood = compute_expectation_step(obs, N, N_ho, N_h1h2, N_h1, N_h, model, debug)
-        loglikelihoods.append(dataset_loglikelihood)
-
-        ### Maximization step ###
-        model = compute_maximization_step(N, M, N_ho, N_h1h2, N_h1, N_h, model, debug)
-        
-
-        # Termination
-        if debug:
-            print_model(model, "After %d iterations" % iters)
-        delta = max_delta(model, old_model)
-        if debug:
-            print "Iters = %d, delta = %f, Log prob of sequences: %f" % (
-            iters, delta, loglikelihoods[-1])
-        sys.stdout.flush()
-
-        iters += 1
-
-        improvement = 1
-        if len(loglikelihoods) > 1:
-            cur = loglikelihoods[-1]
-            prev = loglikelihoods[-2]
-            
-            improvement = (cur-prev) / abs(prev)
-
-        # Two ways to stop: 
-        # (1) the probs stop changing
-        epsilon = 0.001
-        if delta < epsilon:
-            if debug:
-                print "Converged to within %f!\n\n" % epsilon
-            break
-        
-        # (2) the improvement in log likelyhood is too small to bother
-        smaller = 0.0002
-        if improvement < smaller:
-            if debug:
-                print "Converged. Log likelyhood improvement was less that %f.\n\n" % smaller
-            break
-        
-        if num_iters:
-            if iters-1 == num_iters:
-                if debug:
-                    print "Maximum number of iterations (%d iterations) reached.\n\n" % (iters-1)
-                break
-
-    (initial, tran_model, obs_model) = model
-    if not flag:
-        return tran_model, obs_model, initial
-    else:
-        return tran_model, obs_model, initial, loglikelihoods
-
-
-
-
 
 
 class HMM:
@@ -557,21 +287,6 @@ observations = %s
         ans = custom_flatten(argmax)
         ans.reverse()
         return ans
-   
-    def gen_random_sequence(self, n):
-        """
-        Use the underlying model to generate a sequence of 
-        n (state, observation) pairs
-        """
-        # pick a starting point
-        state = random_from_dist(self.initial);
-        obs = random_from_dist(self.observation[state])
-        seq = [(state,obs)]
-        for i in range(1,n):
-            state = random_from_dist(self.transition[state])
-            obs = random_from_dist(self.observation[state])
-            seq.append( (state, obs) )
-        return seq
 
 if __name__ == "__main__":
     test()
