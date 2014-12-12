0dfe6402fce27d17450be9bf5da3acb4673010c6
joshfermin <joshfermin@gmail.com>
2014-12-11 21:45:06 -0700

    assignment 5 done

--- Assignment5/hmm.py
+++ Assignment5/hmm.py
@@ -27,9 +27,6 @@ def array_to_string(a):
     return [str(x) for x in a]
 
 def custom_flatten(xs):
-    """flatten a list that looks like [a,[b,[c,[d,[e]]]]]
-    needed because the list can be hundreds of thousands of elements long,
-    and the recursion in regular flatten can't handle it."""
     result = []
     while len(xs) != 1:
         result.append(xs[0])
@@ -39,16 +36,6 @@ def custom_flatten(xs):
     return result
 
 def flatten(x):
-    """flatten(sequence) -> list
-    Returns a single, flat list which contains all elements retrieved
-    from the sequence and all recursively contained sub-sequences
-    (iterables).
-    Examples:
-    >>> [1, 2, [3,4], (5,6)]
-    [1, 2, [3, 4], (5, 6)]
-    >>> flatten([[[1,2,3], (42,None)], [4,5], [6], 7, MyVector(8,9,10)])
-    [1, 2, 3, 42, None, 4, 5, 6, 7, 8, 9, 10]"""
-
     result = []
     for el in x:
         if hasattr(el, "__iter__") and not isinstance(el, basestring):
@@ -59,9 +46,6 @@ def flatten(x):
 
 def format_array(arr):
     s = shape(arr)
-    if s[0] > MAX_PRINTING_SIZE or (len(s) == 2 and s[1] > MAX_PRINTING_SIZE):
-        return "[  too many values (%s)   ]" % s
-
     if len(s) == 1:
         return  "[  " + (
             " ".join(["%.6f" % float(arr[i]) for i in range(s[0])])) + "  ]"
@@ -105,11 +89,7 @@ def print_model(model, label):
 
 
 class HMM:
-    """ HMM Class that defines the parameters for HMM """
     def __init__(self, states, outputs):
-        """If the hmm is going to be trained from data with labeled states,
-        states should be a list of the state names.  If the HMM is
-        going to trained using EM, states can just be range(num_states)."""
         self.states = states
         self.outputs = outputs
         n_s = len(states)
@@ -121,7 +101,6 @@ class HMM:
         self.observation = zeros([n_s, n_o])
 
     def set_hidden_model(self, init, trans, observ):
-        """ Debugging function: set the model parameters explicitly """
         self.num_states = len(init)
         self.num_outputs = len(observ[0])
         self.initial = array(init)
@@ -133,7 +112,6 @@ class HMM:
         return (self.initial, self.transition, self.observation)
 
     def compute_logs(self):
-        """Compute and store the logs of the model"""
         f = lambda xs: map(log, xs)
         self.log_initial = f(self.initial)
         self.log_transition = map(f, self.transition)
@@ -148,22 +126,8 @@ observations = %s
        " ".join(array_to_string(self.outputs)), 
        string_of_model((self.initial, self.transition, self.observation), ""))
 
-     
-    # declare the @ decorator just before the function, invokes print_timing()
     @print_timing
     def learn_from_labeled_data(self, state_seqs, obs_seqs):
-        """
-        Learn the parameters given state and observations sequences. 
-        Tje ordering of states in states[i][j] must correspond with observations[i][j].
-        Uses Laplacian smoothing to avoid zero probabilities.
-        """
-
-        # Fill this in...
-#       self.initial = normalize(...)
-#       self.transition = ...
-#       self.observation = ...
-#       self.compute_logs()
-        
         prefix = zeros(self.num_states)
         for state in state_seqs:
             self.initial[state[0]] += 1
@@ -190,38 +154,7 @@ observations = %s
         self.compute_logs()
                      
     # declare the @ decorator just before the function, invokes print_timing()
-    @print_timing
-    def learn_from_observations(self, instances, debug=False, flag=False):
-        """
-        Learn hmm parameters based on the specified instances.
-        This would find the maximum likelyhood transition model,
-        observation model, and initial probabilities.
-        """
-        #def baumwelch(obs,N,M, num_iters=0, debug=False,init_model=None, flag=False):   
-        loglikelihoods = None
-        if not flag:
-            (self.transition, 
-             self.observation,
-             self.initial) = baumwelch(instances,
-                                       len(self.states), 
-                                       len(self.outputs), 
-                                       0,
-                                       debug)
-        else:
-            (self.transition, 
-             self.observation,
-             self.initial,
-             loglikelihoods) = baumwelch(instances,
-                                       len(self.states), 
-                                       len(self.outputs), 
-                                       0,
-                                       debug, None, flag)
-            
-        
-        self.compute_logs()
-
-        if flag:
-            return loglikelihoods    
+    @print_timing  
 
     # Return the log probability that this hmm assigns to a particular output
     # sequence
@@ -231,18 +164,11 @@ observations = %s
 
         return loglikelyhood
 
-    def most_likely_states(self, sequence, debug=False):
-        """Return the most like sequence of states given an output sequence.
-        Uses Viterbi algorithm to compute this.
-        """
-        # Code modified from wikipedia
-        # Change this to use logs
-       
+    def viterbi(self, sequence, debug=False):
         cnt = 0
         states = range(0, self.num_states)
         T = {}
         for state in states:
-            ##          V.path   V. prob.
             output = sequence[0]
             p = self.log_initial[state] + self.log_observation[state][output]
             T[state] = ([state], p)
@@ -265,25 +191,16 @@ observations = %s
                     if valmax is None or v_prob > valmax:
                         argmax = v_path
                         valmax = v_prob
-                # Using a nested (reversed) list for performance
-                # reasons: the wikipedia code does a list copy, which
-                # causes problems with long lists.  The reverse is
-                # needed to make the flatten easy.  (This is
-                # essentially using a lisp-like Cons cell representation)
                 argmax = [next_state, argmax]
                 U[next_state] = (argmax, valmax)
             T = U
-        ## apply sum/max to the final states:
         argmax = None
         valmax = None
         for state in states:
             (v_path, v_prob) = T[state]
-#            print "%s  %s" % T[state]
             if valmax is None or v_prob > valmax:
                 argmax = v_path
                 valmax = v_prob
-
-        # Kept the list as in reverse order, and nested to make things fast.
         ans = custom_flatten(argmax)
         ans.reverse()
         return ans
